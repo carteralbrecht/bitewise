6ee1c8bc49d786adb721ba5590794c930cb94bb7
chdero <chrisderosa100@knights.ucf.edu>
2020-11-10 17:25:43 -0500

    added ratings.dart, includes function to write a rating doc in firestore

--- lib/pages/backendTestPage.dart
+++ lib/pages/backendTestPage.dart
@@ -1,4 +1,4 @@
-import 'package:bitewise/services/auth.dart';
+import 'package:bitewise/services/ratings.dart';
 import 'package:flutter/material.dart';
 
 class Test extends StatefulWidget {
@@ -7,80 +7,99 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-
   //Text Field State
   String field1 = '';
-  String field2 = '';
+  num field2 = 0;
 
-  final AuthService _auth = AuthService();
+  final RatingManager _rateMan = RatingManager();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white70,
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[600],
-        title: Text('Test', style: TextStyle(color: Colors.black,  ),),
-        centerTitle: true,
-        
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20),
-              TextFormField(
-                onChanged: (val) {
-                  setState(() {
-                    field1 = val;
-                  });
-                },
-                decoration: InputDecoration(
-                  hintText: 'field 1'
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                onChanged: (val) {
-                  setState(() {
-                    field2 = val;
-                  });
-                },
-                decoration: InputDecoration(
-                  hintText: 'field 2'
-                ),
-              ),
-              SizedBox(height: 20),
-              RaisedButton(
-                onPressed: () async {
-                  // Put function here
-                },
-                child: Text('Test 1',  style: TextStyle(color: Colors.black, fontSize: 20),),
-                color: Colors.yellow[600],
-              ),
-              SizedBox(height: 20),
-              RaisedButton(
-                onPressed: () async {
-                  // put function here
-                },
-                child: Text('Test 2',  style: TextStyle(color: Colors.black, fontSize: 20),),
-                color: Colors.yellow[600],
-              ),
-              SizedBox(height: 20),
-              RaisedButton(
-                onPressed: () async {
-                  // put function here
-                },
-                child: Text('Test 3',  style: TextStyle(color: Colors.black, fontSize: 20),),
-                color: Colors.yellow[600],
-              ),
-              
-            ],
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          backgroundColor: Colors.yellow[600],
+          title: Text(
+            'Test',
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
-        )
-      )
-    );
+          centerTitle: true,
+        ),
+        body: Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+            child: Form(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (val) {
+                      setState(() {
+                        field1 = val;
+                      });
+                    },
+                    decoration: InputDecoration(hintText: 'field 1'),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (val) {
+                      setState(() {
+                        // temp way to make field2 a num
+                        // The form field will be diff in the rating modal
+                        // so this part doesnt need to be big brained
+                        if (val == '1') {
+                          field2 = 1;
+                        } else if (val == '2') {
+                          field2 = 2;
+                        } else if (val == '3') {
+                          field2 = 3;
+                        } else if (val == '4') {
+                          field2 = 4;
+                        } else {
+                          field2 = 5;
+                        }
+                      });
+                    },
+                    decoration: InputDecoration(hintText: 'field 2'),
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    onPressed: () async {
+                      _rateMan.leaveRating(field1, field2);
+                    },
+                    child: Text(
+                      'Test 1',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    color: Colors.yellow[600],
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    onPressed: () async {
+                      // put function here
+                    },
+                    child: Text(
+                      'Test 2',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    color: Colors.yellow[600],
+                  ),
+                  SizedBox(height: 20),
+                  RaisedButton(
+                    onPressed: () async {
+                      // put function here
+                    },
+                    child: Text(
+                      'Test 3',
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    color: Colors.yellow[600],
+                  ),
+                ],
+              ),
+            )));
   }
 }
