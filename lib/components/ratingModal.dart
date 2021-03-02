c2ec420f3e827e37b70a0f76e81c588563319f54
Jesse <jesserehrer@knights.ucf.edu>
2021-03-01 21:22:51 -0500

    Initial changes to rating modal. :)

--- lib/components/ratingModal.dart
+++ lib/components/ratingModal.dart
@@ -23,29 +23,65 @@ class _RatingModalState extends State<RatingModal> {
       elevation: 20,
       content: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Container(
                 child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  widget.menuItem.name,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  widget.restaurant.name,
-                  style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 12,
-                      fontStyle: FontStyle.italic),
-                ),
-              ],
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    widget.menuItem.name,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20),
+                  ),
+                  SizedBox(height: 5),
+                ],
             )),
+            Container(
+              child:
+                Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        constraints: BoxConstraints(maxWidth: 200),
+                        child:
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                widget.menuItem.description,
+                                style: TextStyle(
+                                    color: Colors.black38,
+                                    fontSize: 12),
+                              ),
+                            ]
+                        ),
+                      ),
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(Icons.star_rate_rounded, size: 35, color: Colors.yellow[700]),
+                          ]
+                      ),
+                      SizedBox(width: 5),
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "4.3",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25),
+                            ),
+                          ]
+                      ),
+                    ]
+                ),
+            ),
             Container(
               padding: EdgeInsets.symmetric(vertical: 10),
               child: RatingBar(
@@ -54,9 +90,9 @@ class _RatingModalState extends State<RatingModal> {
                 allowHalfRating: false,
                 itemCount: 5,
                 ratingWidget: RatingWidget(
-                  full: Icon(Icons.circle, color: Colors.amber),
+                  full: Icon(Icons.star_rate_rounded, color: Colors.yellow[700]),
                   half: null,
-                  empty: Icon(Icons.radio_button_off, color: Colors.amber),
+                  empty: Icon(Icons.star_rate_rounded, color: Colors.black26),
                 ),
                 itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
                 onRatingUpdate: (rating) {
