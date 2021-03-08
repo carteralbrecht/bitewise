0c83fcee2460070efcf33e978e59ce6ce7295a7e
chdero <chrisderosa100@knights.ucf.edu>
2021-03-08 17:04:52 -0500

    Stream for avgRating (Plus a bunch of junk, will clean up)

---
+++ lib/models/menuItemFirestore.dart
@@ -0,0 +1,7 @@
+class FirestoreMenuItem {
+  final num avgRating;
+  final num numRatings;
+  final String restaurantId;
+
+  FirestoreMenuItem({this.avgRating, this.numRatings, this.restaurantId});
+}
