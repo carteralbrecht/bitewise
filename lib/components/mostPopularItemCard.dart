001030457bb557adadb54c22bc710d378475ea90
Benjamin Kingsley <60403505+bkingsley727@users.noreply.github.com>
2021-03-11 16:35:24 -0500

    Updates to Top Items

--- lib/components/mostPopularItemCard.dart
+++ lib/components/mostPopularItemCard.dart
@@ -35,36 +35,27 @@ class _MostPopularItemCardState extends State<MostPopularItemCard> {
                     MaterialPageRoute(
                         builder: (context) => RestaurantPage(widget.restaurant)));
         },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget> [
-            Container(
-              child: Row(
-                children: [
-                  Icon(Icons.star, color: global.mainColor, size: 25),
-                  SizedBox(width:5),
-                  Text(widget.avgRating.toStringAsFixed(1), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,)),
-                ],
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Text(
+                widget.menuItem.name,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
-            ),
-            SizedBox(width:10),
-            VerticalDivider(
-              width: 2,
-              thickness: 2,
-              color: global.accentGrayDark,
-              indent: 5,
-              endIndent: 5,
-            ),
-            SizedBox(width:10),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget> [
-                  Text(
-                    widget.menuItem.name,
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Icon(Icons.star, color: global.mainColor, size: 25),
+                        SizedBox(width:5),
+                        Text(widget.avgRating.toStringAsFixed(1), style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,)),
+                      ],
+                    ),
                   ),
                   Text(
                     widget.restaurant.name,
@@ -73,8 +64,8 @@ class _MostPopularItemCardState extends State<MostPopularItemCard> {
                   ),
                 ]
               ),
-            ),
-          ]
+            ]
+          ),
         ),
       ),
     );
