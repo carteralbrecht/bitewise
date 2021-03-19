2f90f838fe343e062de577e1b05feb78e91e1b44
Carter Albrecht <cartera@knights.ucf.edu>
2021-03-19 18:11:53 -0400

    get prev rated items works & make stuff singletons

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,9 +1,11 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'package:bitewise/models/menu.dart';
 import 'package:bitewise/models/menuItem.dart';
 import 'package:bitewise/services/documenu.dart';
 import 'package:bitewise/services/fsmanager.dart';
 import 'package:bitewise/util/geoUtil.dart';
+import 'package:bitewise/util/itemListUtil.dart';
 import 'package:bitewise/util/restaurantUtil.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
@@ -94,6 +96,12 @@ class _HomePageState extends State<HomePage> {
     for (Restaurant restaurant in restaurants) {
       resultsNear.add(restaurant);
       restDistances.add(await GeoUtil.distanceToRestaurant(currentLocation, restaurant));
+    }
+
+    List<Future<MenuItem>> menuItems = await ItemListUtil.getPreviouslyRatedItems();
+
+    for (Future<MenuItem> futureItem in menuItems) {
+      MenuItem item = await futureItem;
     }
 
     setState(() {
