718ee9fb7a57515d1210dd8faa5c2e6e6b4fd9c2
chdero <chrisderosa100@knights.ucf.edu>
2021-04-05 17:14:48 -0400

    formatting

--- lib/components/menuItemSearchTile.dart
+++ lib/components/menuItemSearchTile.dart
@@ -17,19 +17,19 @@ class MenuItemSearchTile extends StatefulWidget {
   final Future milesAway;
   final String id;
 
-  const MenuItemSearchTile(
-      this.menuItem, this.restaurant, this.avgRating, this.milesAway, this.id);
+  const MenuItemSearchTile(this.menuItem, this.restaurant, this.avgRating, this.milesAway, this.id);
 
   @override
   _MenuItemSearchTileState createState() => _MenuItemSearchTileState();
 }
 
 class _MenuItemSearchTileState extends State<MenuItemSearchTile> {
+
   var avgRating;
   var milesAway;
   Restaurant restaurant;
 
-  @override
+  @override 
   void initState() {
     getRestaurant();
     super.initState();
@@ -39,62 +39,54 @@ class _MenuItemSearchTileState extends State<MenuItemSearchTile> {
     Restaurant r = await widget.restaurant;
     var avg = await widget.avgRating;
     var mi = await widget.milesAway;
-    if (mounted) {
-      setState(() {
-        restaurant = r;
-        avgRating = avg;
-        milesAway = mi;
-      });
-    }
+    setState(() {
+      restaurant = r;
+      avgRating = avg;
+      milesAway = mi;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only(bottom: 5, top: 10),
-        height: 100,
-        child: Column(children: <Widget>[
-          Row(children: [
-            Expanded(
-                child: Text(widget.menuItem.name,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold))),
-            Container(
-              child: Row(
-                children: [
-                  Icon(Icons.star, color: global.mainColor, size: 25),
-                  SizedBox(width: 5),
-                  Text(avgRating == null ? "0" : avgRating.toStringAsFixed(1),
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      )),
-                ],
+      margin: EdgeInsets.only(bottom:5, top:10),
+      height: 100,
+      child: Column(
+        children: <Widget> [
+          Row(
+            children: [
+              Expanded(
+                child: Text(widget.menuItem.name, style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold))
               ),
-            ),
-          ]),
-          Row(children: [
-            Expanded(
-              child: Text(
-                restaurant == null ? " " : restaurant.name,
-                style: TextStyle(
-                  fontSize: 15,
-                  fontStyle: FontStyle.italic,
-                  color: global.accentGrayDark,
+              Container(
+                child: Row(
+                  children: [
+                    Icon(Icons.star, color: global.mainColor, size: 25),
+                    SizedBox(width:5),
+                    Text(avgRating == null ? "0" : avgRating.toStringAsFixed(1), style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,)),
+                  ],
                 ),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
               ),
-            ),
-            Container(
-                child: milesAway == null
-                    ? null
-                    : Text(milesAway.toStringAsFixed(2) + " mi",
-                        style: TextStyle(fontSize: 15))),
-          ])
-        ]));
+            ]
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  restaurant == null ? " " : restaurant.name,
+                  style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic, color: global.accentGrayDark,),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Container(
+                child: milesAway == null ? null : Text(milesAway.toStringAsFixed(2) + " mi", style: TextStyle(fontSize: 15))
+              ),
+            ]
+          )
+        ]
+      )
+    );
   }
 }
 
